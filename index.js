6757da2a967f87616d7827d4c46d7fa30606d525
Dawar Ahmed <dawarahmed@Dawars-MacBook-Pro.local>
2018-05-30 15:07:29 +0500

    links fixed

---
+++ index.js
@@ -0,0 +1,15 @@
+function head2() {
+    window.open("https://www.linkedin.com/in/dawar-ahmed/")
+}
+
+function head3() {
+    window.open("https://www.google.com")
+}
+
+function head4() {
+    window.open("https://www.linkedin.com/in/dawar-ahmed/")
+}
+
+function head5() {
+    window.open("https://github.com/dawarahmed")
+}
